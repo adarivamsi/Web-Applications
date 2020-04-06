ac6e8477c7859a8c118c754cc3e994c6386a422b
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-04-05 23:16:14 -0400

    App

---
+++ imad-app/server.js
@@ -0,0 +1,133 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+var adaris = { 
+    'adari1': {
+      title: 'Adari 1 | Adari Vamsi',
+      heading: 'Adari 1',
+      date: '27 August, 2017',
+      content: `
+            <p>
+            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+             <p>
+            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+             <p>
+            This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+             </p>`
+             
+    },
+    'adari2': { 
+              title: 'Adari 2 | Adari Vamsi',
+              heading: 'Adari 2',
+              date: '28 August, 2017',
+                   content: `
+                    <p>
+                    This is the content for my second article.  This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                    </p>
+                    <p>
+                    This is the content for my second article.  This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                    </p><p>
+                    This is the content for my second article.  This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                    </p>`
+    },
+    'adari3': {
+              title: 'Adari 3 | Adari Vamsi',
+              heading: 'Adari 3',
+              date: '29 August, 2017',
+              content: `
+                    <p>
+                    This is the content for my third article.  This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                    </p>
+                    <p>
+                    This is the content for my second article.  This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                    </p><p>
+                    This is the content for my second article.  This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                    </p>`
+    }
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+               ${title}
+            </title>
+            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+              <link href="/ui/style.css" rel="stylesheet" />
+    
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;
+app.get('/counter', function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());   
+});
+
+var names=[];
+app.get('/submit-name',function(req,res){   //URL: /submit-name?name=vamsi
+   //Get the name from the request made by the user
+   var name = req.query.name;
+   
+   names.push(name);
+   
+   res.send(JSON.stringify(names));
+});
+
+app.get('/:adariName', function (req, res){
+    var adariName = req.params.adariName;
+    res.send(createTemplate(adaris[adariName]));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 8080;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
---
+++ imad-app/ui/main.js
@@ -0,0 +1,59 @@
+//Counter Code
+var button = document.getElementById("counter");
+
+button.onclick = function(){
+
+  //Create a request object
+   var request = new XMLHttpRequest();
+
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        //Take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML= counter.toString();
+        }
+    }
+        //Not done yet
+  };
+
+    //Make a request
+    request.open('GET','http://localhost:8080/counter',true);
+    request.send(null);
+};
+
+//Submit Name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+
+         //Create a request object
+   var request = new XMLHttpRequest();
+
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        //Take some action
+        if(request.status === 200){
+              //capture a list of names and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML=list;
+        }
+    }
+        //Not done yet
+  };
+
+    //Make a request
+    var nameInput =  document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://localhost:8080/submit-name?name='+name,true);
+    request.send(null);
+
+};
